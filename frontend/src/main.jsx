7ee9b396b2698cc3f7c8b74345f6fd44bdfc97e3
Mario Allesandro <mariorio290@gmail.com>
2026-07-12 18:32:43 +0700

    Self-host boxicons, fix missing react-helmet-async dependency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,17 @@
-﻿import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import { ToastProvider } from "./context/ToastContext.jsx";
 import App from "./App.jsx";
+import "boxicons/css/boxicons.min.css";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <ToastProvider>
+        <App />
+      </ToastProvider>
+    </HelmetProvider>
+  </React.StrictMode>
 );
